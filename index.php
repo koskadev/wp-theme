da1823a8f393aff3a3ac07a14a604e133a854ac0
Aloasfire <Aloasfire@icloud.com>
2015-11-27 19:25:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
+<?php
+if(have_posts()){
+  while(have_posts()) : the_post();
+  echo(<h2>the_title())</h2>;
+  echo(the_content());
+  end while
+  else:
+    echo'<p>No Posts Found !<p>';
+    endif;
+}
 
+
+
+?>
