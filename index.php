428ea9fa73af361e46439491c049f8d0ff64c597
Aloasfire <Aloasfire@icloud.com>
2015-11-27 19:27:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 if(have_posts()){
   while(have_posts()) : the_post();
-  echo(<h2>the_title())</h2>;
-  echo(the_content());
+  echo(<h2><a href=the_permalink();>the_title()</a></h2>);
+  echo(the_content(););
   end while
   else:
     echo'<p>No Posts Found !<p>';
