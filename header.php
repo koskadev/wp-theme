aa4519f264ee3fb7d8e2e2d52fb79e1aaddff9fa
Aloasfire <Aloasfire@icloud.com>
2015-11-28 16:48:14 -0800

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
   <head>
-    
+    <meta charset="<?php bloginfo('charser'); ?>">
+    <meta name="viewport" content="wight=device-wight">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
   </head>
   <body>
