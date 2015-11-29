9ccc2dfbbb8e2d0e7adddf1a003c5096739460af
Aloasfire <Aloasfire@icloud.com>
2015-11-28 17:34:27 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 get_header();
 if(have_posts()){
   while(have_posts()) : the_post();
+  
+  <article clas="post">
   echo(<h2><a href=the_permalink();>the_title()</a></h2>);
   echo(the_content(););
+  </article>
   end while
   else:
     echo'<p>No Posts Found !<p>';
